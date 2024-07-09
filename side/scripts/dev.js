5c918b02693fe22bd0ca9132fd366b7c6fb485bf
Muhammad Usman <usmansaleem4446996@gmail.com>
2024-07-09 12:00:39 +0500

    copy function for http added

--- side/scripts/dev.js
+++ side/scripts/dev.js
@@ -71,6 +71,29 @@ function copy(word) {
     navigator.clipboard.writeText(word);
 }
 
+//for http
+/*function copy(text) {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        const successful = document.execCommand('copy');
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}*/
+
 async function PngToJpg() {
 
     //showing that request is being proceded
